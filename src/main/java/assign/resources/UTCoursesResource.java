3ae46d8e1890d5a7a611217360a083733046a5ca
saleh <almto3@gmail.com>
2016-10-18 12:40:04 -0500

    almost working, need some tweaking

--- src/main/java/assign/resources/UTCoursesResource.java
+++ src/main/java/assign/resources/UTCoursesResource.java
@@ -3,7 +3,7 @@ package assign.resources;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -14,69 +14,62 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
-import assign.domain.Course;
-import assign.domain.Courses;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import assign.domain.Project;
 import assign.domain.Projects;
 import assign.services.EavesdropService;
 
-@Path("/listing")
+@Path("/myeavesdrop")
 public class UTCoursesResource {
 	
 	EavesdropService eavesdropService;
-	
+	final String URL = "http://localhost:8080/assignment3/myeavesdrop/projects/";
+	final String ext = "meetings";
+	private Project project = new Project();
+	private Projects projects = new Projects();
+	private EavesdropService ser = null;
+
 	public UTCoursesResource() {
-		this.eavesdropService = new EavesdropService();
+		System.out.println("-- UTCoursesResource constructor");
+		startService();
+	}
+	
+	/* EAVESDROP	*/
+	void setEavesdropService(EavesdropService ser){
+		this.ser = ser;
+	}
+	EavesdropService startService(){
+		ser =  EavesdropService.getEavesdrop();
+		return ser;
+	}
+	@GET
+	@Path("/eavesdrophelloworld")
+	@Produces("text/html")
+	public String helloEaves() {
+		System.out.println("-- UTCoursesResource helloEaves");
+		return ser.getHello();		
 	}
 	
 	@GET
 	@Path("/helloworld")
 	@Produces("text/html")
 	public String helloWorld() {
+		System.out.println("-- UTCoursesResource helloWorld");
 		return "Hello world";		
 	}
-	
-	@GET
-	@Path("/helloeavesdrop")
-	@Produces("text/html")
-	public String helloEavesdrop() {
-		return this.eavesdropService.getData();		
-	}	
-	
-	@GET
-	@Path("/courses")
-	@Produces("application/xml")
-	public StreamingOutput getAllCourses() throws Exception {
-		Course modernWebApps = new Course();
-		modernWebApps.setDepartment("CS");
-		modernWebApps.setName("Modern Web Applications");
-		
-		Course operatingSystems = new Course();
-		operatingSystems.setDepartment("CS");
-		operatingSystems.setName("Operating Systems");
-		
-		final Courses courses = new Courses();
-		List<Course> courseList = new ArrayList<Course>();
-		courseList.add(modernWebApps);
-		courseList.add(operatingSystems);
-		courses.setCourses(courseList);		
-			    
-	    return new StreamingOutput() {
-	         public void write(OutputStream outputStream) throws IOException, WebApplicationException {
-	            outputCourses(outputStream, courses);
-	         }
-	      };	    
-	}
-	
 	@GET
 	@Path("/projects")
 	@Produces("application/xml")
-	public StreamingOutput getAllProjects() throws Exception {
-		Project heat = new Project();
-		heat.setName("%23heat");
-				
+	public String getAllProjects() throws IOException {
+		Document doc = ser.getDoc(URL);
+		Elements elems = ser.getProjects(doc);
+		elems.addAll(ser.getProjects(doc));
+		return ser.getData(elems);
+		/*
 		final Projects projects = new Projects();
-		projects.setProjects(new ArrayList<String>());
+		projects.setProjects(new HashSet<String>());
 		projects.getProjects().add("%23heat");
 		projects.getProjects().add("%23dox");		
 			    
@@ -84,12 +77,9 @@ public class UTCoursesResource {
 	         public void write(OutputStream outputStream) throws IOException, WebApplicationException {
 	            outputCourses(outputStream, projects);
 	         }
-	      };	    
+	      };
+	    */	    
 	}	
-	
-	@GET
-	@Path("/project")
-	@Produces("application/xml")
 	public StreamingOutput getProject() throws Exception {
 		final Project heat = new Project();
 		heat.setName("%23heat");
@@ -102,21 +92,7 @@ public class UTCoursesResource {
 	            outputCourses(outputStream, heat);
 	         }
 	      };	    
-	}		
-	
-	protected void outputCourses(OutputStream os, Courses courses) throws IOException {
-		try { 
-			JAXBContext jaxbContext = JAXBContext.newInstance(Courses.class);
-			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-	 
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			jaxbMarshaller.marshal(courses, os);
-		} catch (JAXBException jaxb) {
-			jaxb.printStackTrace();
-			throw new WebApplicationException();
-		}
 	}
-	
 	protected void outputCourses(OutputStream os, Projects projects) throws IOException {
 		try { 
 			JAXBContext jaxbContext = JAXBContext.newInstance(Projects.class);
